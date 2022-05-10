020e90d87fb4add074e5016c204314395b0f0428
Peter Rockett <p.rockett@sheffield.ac.uk>
2022-05-10 12:57:25 +0100

    Modified Python scripts and make-datasets script to accomodate different naming conventions for Python 3

--- regression/make-datasets.sh
+++ regression/make-datasets.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Generate data files -- pir -- 16.3.2022
+# Modified to use Python variable -- pir -- 10.5.2022
+
+PYTHON=python3
 
 for i in $(ls -d */)
 	do
@@ -14,10 +17,10 @@ for i in $(ls -d */)
 			if [[ $j != "qsar_aquatic_toxicity" ]]
 				then
 					chmod 700 format-$j-data.py
-					./format-$j-data.py
+					$PYTHON format-$j-data.py
 				else
 					chmod 700 format-aquatic-toxicity-data.py
-					./format-aquatic-toxicity-data.py
+					$PYTHON format-aquatic-toxicity-data.py
 				fi
 			cd ..
 		fi
--- regression/qsar_aquatic_toxicity/format-aquatic-toxicity-data.py
+++ regression/qsar_aquatic_toxicity/format-aquatic-toxicity-data.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python3
-
 #*******************************************************************************
 #
 #  This program is free software; you can redistribute it and/or modify
